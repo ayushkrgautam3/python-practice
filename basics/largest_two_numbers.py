4b363ac7a49f5531668a7f3eb08991d791eb8d8f
ayushkrgautam3 <ayushkrgautam3@gmail.com>
2026-01-11 21:18:40 +0530

    largest_two_numbers.py

---
+++ basics/largest_two_numbers.py
@@ -0,0 +1,7 @@
+#Largest number between two numbers
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+if a>b:
+  print(a,'is largest number.')
+else:
+  print(b,'is largest number.')
